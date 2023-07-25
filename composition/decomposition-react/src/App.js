c1e5cfeabfd201ee0f693f67cdf00a868e337a0f
Polina Fomicheva <puffyoluv@yandex.ru>
2023-07-25 16:44:11 +0300

    decomposition done

---
+++ composition/decomposition-react/src/App.js
@@ -0,0 +1,21 @@
+
+import Ads from './Ads';
+import './App.css';
+import CardInfo from './CardInfo';
+import MainNews from './MainNews';
+import Nav from './Nav';
+import Search from './Search';
+
+function App() {
+  return (
+    <>
+    <MainNews></MainNews>
+    <Nav></Nav>
+    <Search></Search>
+    <CardInfo ></CardInfo>
+    <Ads></Ads>
+    </>
+  );
+}
+
+export default App;
