a1dab9cd0cc96976fda25f2f6870355e6779aa22
Polina Fomicheva <puffyoluv@yandex.ru>
2023-08-03 17:34:07 +0300

    watches done

---
+++ lifecycle-http/watches-react/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Watch from './Watch';
+import Form from './Form';
+
+function App() {
+  return (
+    <>
+    <Form></Form>
+    <Watch></Watch>
+    </>
+  );
+}
+
+export default App;
