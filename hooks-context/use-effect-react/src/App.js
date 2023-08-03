78ce52ecc17a29aa5115d15f0fadb2fd5964a30c
Polina Fomicheva <puffyoluv@yandex.ru>
2023-08-03 20:53:54 +0300

    useeffectdone

---
+++ hooks-context/use-effect-react/src/App.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import List from './List';
+import Details from './Details';
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [activeUser, setActiveUser] = useState(null);
+
+  useEffect(() => {
+    fetch(process.env.REACT_APP_USERS)
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const hendleClickUser = (id, name) => {
+    setActiveUser({id: id, name: name});
+  };
+
+  return (
+    <>
+      <div className='container row mx-auto'>
+        <List
+          users={users}
+          hendleClickUser={hendleClickUser}
+          activeUser={activeUser}
+        />
+        {activeUser && <Details info={activeUser} />}
+        
+      </div>
+    </>
+  );
+}
+
+export default App;
