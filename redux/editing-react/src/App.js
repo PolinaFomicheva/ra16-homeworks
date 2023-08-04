7ff1c6c6a83055970d03de97f73f106e8c927429
Polina Fomicheva <puffyoluv@yandex.ru>
2023-08-04 16:53:27 +0300

    done

---
+++ redux/editing-react/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import ServiceAddList from "./components/ServiceAddLIst";
+import ServiceList from "./components/ServiceList";
+
+function App() {
+  return (
+    <>
+      <ServiceAddList />
+      <ServiceList />
+    </>
+  );
+}
+
+export default App;
---
+++ redux/editing-react/src/components/ItemPrice.js
@@ -0,0 +1,10 @@
+export default function ItemPrice({ item, deleteClick, changeForm }) {
+    return (
+      <li>
+        {item.name} {item.price}
+        <button onClick={() => deleteClick(item.id)}>удалить</button>
+        <button onClick={() => changeForm(item.id)}>изменить</button>
+      </li>
+    );
+  }
+  
